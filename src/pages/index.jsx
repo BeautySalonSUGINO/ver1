3606da73d873784cae393dff99fa5ad74ab70b6c
makotty <hal.m.90215@gmail.com>
2022-01-02 00:00:32 +0900

    BaseLayoutの作成

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import BaseLayout from "../components/BaseLayout"
+
+export default function Home() {
+  return (
+    <BaseLayout>
+      <div>Hello world!</div>
+    </BaseLayout>
+  )
+}
