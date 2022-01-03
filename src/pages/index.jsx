a54b7611ed9a164ef2059e9d75b87280b0dc0924
makotty <hal.m.90215@gmail.com>
2022-01-03 22:28:24 +0900

    トップページにSEOを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+
 import BaseLayout from "../components/BaseLayout"
+
+import Seo from "../containers/Seo"
 
 const Index = () => {
   return (
     <BaseLayout>
+      <Seo />
       <div>Hello world!</div>
     </BaseLayout>
   )
