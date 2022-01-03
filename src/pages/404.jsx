6c36a967ee3f357a8ec5d760c0441c697237f45a
makotty <hal.m.90215@gmail.com>
2022-01-04 00:14:38 +0900

    pagepathを指定

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,10 +4,10 @@ import BaseLayout from "../components/BaseLayout"
 
 import Seo from "../containers/Seo"
 
-const NotFound = () => {
+const NotFound = ({ location }) => {
   return (
     <BaseLayout>
-      <Seo pagetitle="ページが見つかりません" />
+      <Seo pagetitle="ページが見つかりません" pagepath={location.pathname} />
       <div>404:ページが見つかりません</div>
     </BaseLayout>
   )
