4eb084fbdca174dcf0ded354bb65692231ceb031
makotty <hal.m.90215@gmail.com>
2022-01-07 02:11:47 +0900

    記事のタイトルを出力

--- src/pages/blogpost.jsx
+++ src/pages/blogpost.jsx
@@ -1,9 +1,24 @@
 import React from "react"
 
+import { graphql } from "gatsby"
+
 import BaseLayout from "../components/BaseLayout"
 
-const BlogPost = () => {
-  return <BaseLayout></BaseLayout>
+const BlogPost = ({ data }) => {
+  const contentfulNews = data.contentfulNews
+  return (
+    <BaseLayout>
+      <h2>{contentfulNews.title}</h2>
+    </BaseLayout>
+  )
 }
 
 export default BlogPost
+
+export const query = graphql`
+  query {
+    contentfulNews {
+      title
+    }
+  }
+`
