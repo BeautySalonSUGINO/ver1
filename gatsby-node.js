7af4a06915bb8f0dc80266d9b961c5b1ea404199
makotty <hal.m.90215@gmail.com>
2022-01-10 17:47:08 +0900

    ブログページの自動生成

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,33 @@
-const path = requiew("path")
+const path = require("path")
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
+
+  const blogresult = await graphql(`
+    query {
+      allContentfulBlog(sort: { fields: publishDate, order: DESC }) {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  if (blogresult.errors) {
+    reporter.panicOnBuild(`GraphQLのクエリでエラーが発生しました`)
+    return
+  }
+
+  blogresult.data.allContentfulBlog.edges.forEach(({ node }) => {
+    createPage({
+      path: `/blog/${node.slug}`,
+      component: path.resolve(`./src/templates/blogpost.jsx`),
+      context: {
+        id: node.id,
+      },
+    })
+  })
 }
