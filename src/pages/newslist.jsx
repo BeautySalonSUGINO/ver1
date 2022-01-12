2c02d430953e5c591728ca597f50e584b0324583
makotty <hal.m.90215@gmail.com>
2022-01-13 01:29:52 +0900

    ニュース一覧ページの作成

---
+++ src/pages/newslist.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+
+import { graphql, Link } from "gatsby"
+
+import BaseLayout from "@components/BaseLayout"
+
+import Seo from "@containers/Seo"
+
+const NewsList = ({ data, location }) => {
+  return (
+    <BaseLayout>
+      <Seo
+        pagetitle="ニュース一覧"
+        pagedesc="スギノ美容室のニュース一覧です。"
+        pagepath={location.pathname}
+      />
+      <section>
+        {data.allContentfulNews.edges.map(({ node }) => {
+          return (
+            <Link to={`/news/${node.slug}`} key={node.id}>
+              <h3>{node.title}</h3>
+            </Link>
+          )
+        })}
+      </section>
+    </BaseLayout>
+  )
+}
+
+export default NewsList
+
+export const query = graphql`
+  query {
+    allContentfulNews(sort: { order: DESC, fields: publishDate }) {
+      edges {
+        node {
+          title
+          id
+          slug
+        }
+      }
+    }
+  }
+`
