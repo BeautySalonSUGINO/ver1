dc9362687c3ad74856e7aa4f8c2f89340dcc5f61
makotty <hal.m.90215@gmail.com>
2022-01-13 10:20:59 +0900

    NewsAreaへのデータの受け渡し

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,41 @@
 import React from "react"
 
+import { graphql } from "gatsby"
+
 import TopMainVisualHeader from "@components/TopMainVisualHeader"
-import NewsArea from "@components/NewsArea"
+
+import NewsArea from "@containers/NewsArea"
 
 import Seo from "@containers/Seo"
 
-const Index = () => {
+const Index = ({ data }) => {
   return (
     <>
       <Seo />
       <TopMainVisualHeader />
-      <NewsArea />
+      <NewsArea data={data} />
     </>
   )
 }
 
 export default Index
+
+export const query = graphql`
+  query {
+    allContentfulNews(
+      sort: { order: DESC, fields: publishDate }
+      skip: 0
+      limit: 4
+    ) {
+      edges {
+        node {
+          title
+          id
+          slug
+          publishDate
+          publishDateJP: publishDate(formatString: "YYYY/MM/DD")
+        }
+      }
+    }
+  }
+`
