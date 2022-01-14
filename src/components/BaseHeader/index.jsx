6834de4612568a2a33dd1696ad4007d2121dd53b
makotty <hal.m.90215@gmail.com>
2022-01-14 12:37:04 +0900

    Headerコンポ―ネントの更新

--- src/components/BaseHeader/index.jsx
+++ src/components/BaseHeader/index.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 
-const BaseHeader = () => {
-  return <footer>Beauty Salon SUGINO</footer>
+import "@styles/Layout/_header.scss"
+
+const BaseHeader = ({ text }) => {
+  return (
+    <header id="l-header">
+      <h1 id="l-header--h1" className="u-fs__2-5rem">
+        {text}
+      </h1>
+    </header>
+  )
 }
 
 export default BaseHeader
